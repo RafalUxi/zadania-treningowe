111faaabff071917783f7011b71fe98f6adc5122
Rafal <rafal.trzeciakowski9090@o2.pl>
2026-08-20 12:56:18 +0200

    Roman numerals

---
+++ fundament/toRoman.ts
@@ -0,0 +1,30 @@
+const base: [number, string][] = [
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+];
+
+function toRoman(n: number): string {
+  let input = n;
+  let output = "";
+  for (let i = 0; i < base.length; i++) {
+    while (input >= base[i][0]) {
+      input -= base[i][0];
+      output += base[i][1];
+    }
+  }
+
+  return output;
+}
+
+console.log(toRoman(23));
